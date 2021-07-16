904e5f02486aeadad8be24430a4e5f345e71bd74
Brad Campbell <bfcampbell88@gmail.com>
2021-07-16 17:42:49 -0500

    Improvements to iMessage.py

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # iMessageManipulation
 Using Python and Apple Script to send iMessages.
+
+
+
+# Getting Started
+1. Ensure you have Python3 (3.6+) installed.
+
+2. Find your iMessage service ID with the following Applescript.
+    ```
+    tell application "Messages"
+        get every account
+    end tell
+    ```
+
+3. Copy and paste that service ID into the serviceID variable on line 12.
+
+4. Navigate to the project's directory and run the following command.
+    ```
+    python3 iMessage.py
+    ```
+    
+5. Input the desired message and phone number.
--- iMessage.py
+++ iMessage.py
@@ -5,70 +5,50 @@
 ##############################
 
 
+
 import subprocess
 
-# This message will be split up by spaces
-fullMessage = "Test Message. Ignore."
-# Declare phone number EXACTLY as they are inputted into the address book
-phoneNumber = +1 (111) 111-1111
-# The array of words split up by words
-eachWordArray = []
+# Put service ID here
+serviceID = ""
+# Message to be sent
+fullMessage = input("Message you wish to send: \n")
+# Declare phone number EXACTLY as is is stored in the user's address book
+phoneNumber = input("Phone number of intended target (exactly as it is stored in address book: \n")
 
 
-# Takes the inputted string (variable named "fullMessage") and splits up each word into an array (variable "eachWordArray")
-def splitWords():
-    global fullMessage, eachWordArray
-    temp = ""
-    fullMessage += " " # Ensures the last word is printed effectively
 
-    for x in range(0, len(fullMessage), 1):
-        if (fullMessage[x] == " "):
-            #temp += "\n" # Adds line for printing individually
-            eachWordArray.append(str(temp))
-            temp = ""
-        elif (fullMessage[x] == "," or fullMessage[x] == "."):
-            temp += ""
-        else:
-            temp += fullMessage[x]
+# Deploys the text message
+def sendToTarget():
+    global fullMessage, phoneNumber
 
-# Deploys a separate text message for each word of the "eachWordArray"
-def spamTarget():
-    global eachWordArray, phoneNumber
-    
-    for x in range(0, len(eachWordArray), 1):
-        # Script is written in Apple Script language, executable in Script application
-        script = '''tell application "Messages"
-            get every service #prints the service number to print in line below
-            send "''' + str(eachWordArray[x]) + '''" to buddy "''' + str(phoneNumber) + '''" of service id "700D7248-40A4-40E0-8387-0254B2CE75A7"
-        end tell
-        '''
-        proc = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        stdout_output = proc.communicate(script)[0]
-        print ("Completed word " + str(x) + "\t" + str(stdout_output)) # gives running word count and tells error words
+    # Script is written in Apple Script language, executable in Script application
+    script = 'tell application "Messages" get every service send "' + fullMessage + '" to buddy "' + phoneNumber + '" of service id "' + serviceID + '" end tell'
+    proc = subprocess.Popen(['osascript', '-'], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = False)
+    stdout_output, stderr_output = proc.communicate(script)
+    print(stdout_output)
+    print(stderr_output)
+
+
 
 # Runs both mentioned methods and has "print()" check points for console verification
 def main():
-    splitWords()
-    print("Completed initial \"splitWords()\" method.") # Checkpoint
-    spamTarget()
-    print("Completed whole \"spamTarget()\" method.") # Checkpoint
+    try: 
+        sendToTarget()
+        print("The below message has been successfully sent.")
+        print(fullMessage)
+    except:
+        print("Something has gone wrong. Message failed to send.")
 
 
-# Final Method Run
-main()
+
+if __name__ == "__main__":
+    main()
 
 
-# Script reference code below
+
+# Script reference code below for getting service ID
 """
-import subprocess
-script = '''tell application "Messages"
-    get every service #prints the service number to print in line below
-    send "Test Message" to buddy "+15555555555" of service id "PRINTED-BY-LINE-ABOVE"
+tell application "Messages"
+	get every account
 end tell
-'''
-
-proc = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-stdout_output = proc.communicate(script)[0]
-
-print (stdout_output)
 """
